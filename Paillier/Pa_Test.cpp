27ce7d8527d918bfe9e7f0dc435b5a39f2854b73
oksuman <rlatmdgn77@cau.ac.kr>
2023-03-06 12:02:35 +0900

    수행 시간 측정 세분화

--- Paillier/Pa_Test.cpp
+++ Paillier/Pa_Test.cpp
@@ -11,14 +11,30 @@ using namespace std;
 using namespace chrono;
 
 int main(int argc, char* argv[]){
+    srand((unsigned int)time(NULL));
 
         /*
                 Paillier based Equality Protocol Test
                 message : 0 ~ 2047 (char *)
         */
+
+
     system_clock::time_point Pa_total_start_time = system_clock::now();
+        // Initial Step
+    system_clock::time_point Pa_initial_start_time = system_clock::now();    
+    PAILLIER pa = PAILLIER();
+    PK pa_pk;
+    SK pa_sk;
+    pa.KeyGen(pa_pk,pa_sk);
+
+    system_clock::time_point Pa_initial_end_time = system_clock::now();
+    milliseconds pa_initial = duration_cast<milliseconds>(Pa_initial_end_time - Pa_initial_start_time);
+    
+    chrono::duration<double> Pa_step1(0);
+    chrono::duration<double> Pa_step2(0);
+    chrono::duration<double> Pa_step3(0);
+
     for(int k=0; k<1000; k++){
-        srand((unsigned int)time(NULL));
         int m_Alice = rand() % 2048;
         int m_Bob = rand() % 2048;
         
@@ -69,27 +85,37 @@ int main(int argc, char* argv[]){
         }
         
   
-        // Initial Step
-        PAILLIER pa = PAILLIER();
-        PK pa_pk;
-        SK pa_sk;
-        pa.KeyGen(pa_pk,pa_sk);
-       
+        system_clock::time_point Pa_step1_start_time = system_clock::now();  
         // // Step 1
         unsigned char *c_Alice = pa.Enc(pa_pk, hex_m_Alice);
+        system_clock::time_point Pa_step1_end_time = system_clock::now();    
+        Pa_step1 += Pa_step1_end_time-Pa_step1_start_time;
 
+        system_clock::time_point Pa_step2_start_time = system_clock::now();  
         // // Step 2
         unsigned char *c_Bob = pa.Enc(pa_pk, hex_m_Bob);
         unsigned char *sub_result = pa.Sub(pa_pk, c_Alice, c_Bob);
         unsigned char *s = pa.generate_random_element2(pa_pk);
         unsigned char *c_result = pa.Scalar_Mul(pa_pk, s, sub_result);
+        system_clock::time_point Pa_step2_end_time = system_clock::now();  
+        Pa_step2 += Pa_step2_end_time-Pa_step2_start_time;
 
+        system_clock::time_point Pa_step3_start_time = system_clock::now();  
         // // Step 3
         unsigned char *res = pa.Dec(pa_pk, pa_sk, c_result);
+        system_clock::time_point Pa_step3_end_time = system_clock::now();  
+        Pa_step3 += Pa_step3_end_time-Pa_step3_start_time;
 
     }
     system_clock::time_point Pa_total_end_time = system_clock::now();
     milliseconds milli_pa = duration_cast<milliseconds>(Pa_total_end_time - Pa_total_start_time);
+    milliseconds pa_step1 = duration_cast<milliseconds>(Pa_step1);
+    milliseconds pa_step2 = duration_cast<milliseconds>(Pa_step2);
+    milliseconds pa_step3 = duration_cast<milliseconds>(Pa_step3);
+    cout << "initial : " << pa_initial.count() << "ms" << endl;
+    cout << "ece_step1 : " << pa_step1.count() << "ms" << endl;
+    cout << "ece_step2 : " << pa_step2.count() << "ms" << endl;
+    cout << "ece_step3 : " << pa_step3.count() << "ms" << endl;
     cout << milli_pa.count() << "ms" << endl;
 }
 
